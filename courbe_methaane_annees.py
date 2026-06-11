e5b638bd416b33ba1871e1b7432642739e9e50a1
Oihana28 <oihadag4206@gmail.com>
2026-06-11 23:28:04 +0200

    Update courbe_methaane_annees.py

--- courbe_methaane_annees.py
+++ courbe_methaane_annees.py
@@ -6,24 +6,22 @@ import matplotlib.pyplot as plt
 c = 300
 a = 5.07
 
-# Calcul de b pour raccord parfait
+# point d'intersection des deux courbes
 b = 300 - a * 1985
 
-# Intervalles (années réelles)
+# itervalles des deux morceaux de courbe
 x1 = np.linspace(1800, 1985, 100)
 x2 = np.linspace(1985, 2050, 100)
 
-# Fonctions
+# fonctions
 y1 = np.full_like(x1, c)
 y2 = a * x2 + b
 
-# Tracé
+# graph
 plt.plot(x1, y1,)
 plt.plot(x2, y2,)
 
 
-
-# Mise en forme
 plt.xlabel("Année")
 plt.ylabel("Concentration CH₄ (ppb)")
 plt.title("Évolution du méthane")
