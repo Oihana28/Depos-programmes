b33f79f7da1d3db1db2988d692635f50e5e0f58e
Oihana28 <oihadag4206@gmail.com>
2026-06-20 15:25:26 +0200

    Add altitude-based CH4 concentration function

--- concentration en CH4 pour une année donnée.py
+++ concentration en CH4 pour une année donnée.py
@@ -8,4 +8,15 @@ def concentration_CH4(annee):
     else:
         return  5.07 * annee - 8.38 *( 10**3)  # modèle linéaire post-1952
     
-print (concentration_CH4(1953))
+
+def concentration_CH4_altitude(altitude):
+    """
+    Renvoie la proportion estimée de CH4 en ppm pour une altitude donnée.
+    """
+    if  altitude < 9:
+        return 1800                          # valeur à basse altitude en ppb
+    
+    elif  9 <= altitude <= 45:
+        return   -45.2 * altitude + 2190     #valeur à haute altitude
+    else: 
+        return 100                            #valeur avec la fonction affine
